a9135c4fd004273a529859b4ae82302a9d07b139
Bartosz Żbikowski <zbiku112@op.pl>
2017-06-16 23:33:37 +0200

    parse json

---
+++ c6f477302db24e81b0df88b16544ca6e/src/main/scala/Point.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by User on 16/06/2017.
+  */
+case class Point(
+                x1 : Long,
+                y1 : Long,
+                x2 : Long,
+                y2 : Long
+                )
--- c6f477302db24e81b0df88b16544ca6e/src/main/scala/Start.scala
+++ c6f477302db24e81b0df88b16544ca6e/src/main/scala/Start.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.sql.SparkSession
 
+import scala.collection.mutable
 
 class Start {
   val sparkSession: SparkSession = SparkSession.builder.
@@ -9,6 +10,12 @@ class Start {
 
   def getData : Unit = {
     val data = sparkSession.read.json("./src/main/resources/dataMay-31-2017.json")
-    data.show()
+    //val keys_array = data.first().get(0).asInstanceOf[mutable.WrappedArray[String]]
+    val values_array = data.first().get(1).asInstanceOf[mutable.WrappedArray[mutable.WrappedArray[Long]]]
+    val array = new Array[Point](values_array.length)
+    var i = 0
+    values_array.foreach{ value => array.update(i, Point(value.apply(0),value.apply(1),value.apply(2),value.apply(3)))
+                          i += 1;}
+    array.foreach{ println }
   }
 }
