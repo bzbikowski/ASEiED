cb55c32efedbb36039f15e2d4d022f1be6e9ace8
Bartosz Żbikowski <zbiku112@op.pl>
2017-06-24 23:38:10 +0200

    poprawki

--- c6f477302db24e81b0df88b16544ca6e/src/main/scala/Start.scala
+++ c6f477302db24e81b0df88b16544ca6e/src/main/scala/Start.scala
@@ -1,5 +1,4 @@
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
 import scala.collection.mutable
@@ -10,26 +9,25 @@ class Start {
     .appName("spark project")
     .getOrCreate()
 
-  def getData : Unit = {
+  def getData() : Unit = {
     val file = "./src/main/resources/dataMay-31-2017.json"
     val parsed = sparkSession.sparkContext.wholeTextFiles(file).values
     val data = sparkSession.read.json(parsed)
     val points = data.withColumn("data", explode(data.col("data"))).select("data")
     val array = new Array[Point](points.count().asInstanceOf[Int])
-    var i = 0
-    points.foreach { value => var temp = value.apply(0).asInstanceOf[mutable.WrappedArray[Long]]
-      array.update(i, Point(temp.apply(0), temp.apply(1), temp.apply(2), temp.apply(3))) // nie podmienia wartości
-      i += 1;
+    val list = points.collectAsList()
+    for(i <- 0 until list.size()){
+      val temp = list.get(i).apply(0).asInstanceOf[mutable.WrappedArray[Long]]
+      array.update(i, Point(temp.apply(0), temp.apply(1), temp.apply(2), temp.apply(3)))
     }
-    array.foreach(println)
     val team1_apriori = array.length.toFloat / (2 * array.length)
     val team2_apriori = array.length.toFloat / (2 * array.length)
     val target = Target_point(30, 30)
-    val radius = 15
+    val radius = 20
     var team1 = 0
     var team2 = 0
-    array.foreach { item => if(check_blue(target, radius, item)) team1 += 1}
-    array.foreach { item => if(check_red(target, radius, item)) team2 += 1}
+    array.foreach { item => if(check_team(target, radius, item, team = true)) team1 += 1}
+    array.foreach { item => if(check_team(target, radius, item, team = false)) team2 += 1}
     val team1_postpriori = team1 / array.length.toFloat
     val team2_postpriori = team2 / array.length.toFloat
     val prawd_team1 = team1_apriori * team1_postpriori
@@ -38,23 +36,21 @@ class Start {
     println("Prawdopodobienstwo nr 2 : " + prawd_team2 * 100 + " %")
   }
 
-  def check_blue (target:Target_point, radius:Int, item:Point) : Boolean = {
-    val punkt = Target_point(item.x1, item.y1)
+  def check_team (target:Target_point, radius:Int, item:Point, team:Boolean) : Boolean = {
+    var punkt = Target_point(30, 30)
+    if(team) {
+      punkt = Target_point(item.x1, item.y1)
+    }
+    else {
+      punkt = Target_point(item.x2, item.y2)
+    }
     if(Math.sqrt(pow(punkt.x - target.x) + pow(punkt.y - target.y)) < radius)
       true
     else
       false
   }
 
-  def check_red (target:Target_point, radius:Int, item:Point) : Boolean = {
-    val punkt = Target_point(item.x2, item.y2)
-    if(Math.sqrt(pow(punkt.x - target.x) + pow(punkt.y - target.y)) < radius)
-      true
-    else
-      false
-  }
-
-  def pow(l : Long) : Long = {
-    l * l
+  def pow(v : Long) : Long = {
+    v * v
   }
 }
